ee50984aba398c8618a512c05cf48c27a3da2bcd
Yaroslav Ilnitskiy <eyarij@gmail.com>
2020-09-23 11:21:45 +0300

    student

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace student
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter Name");
+            string name = Console.ReadLine();
+
+            Student student = new Student(name);
+
+            Console.WriteLine("Enter Year");
+            student.Year = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Phone");
+            student.Phone = Console.ReadLine();
+
+            Console.WriteLine("Enter Gender");
+            student.Gender = Console.ReadLine();
+
+            Console.WriteLine("Student successfully added");
+
+            Console.WriteLine(student.Output());
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace student
+{
+    public class Student
+    {
+        private string name;
+        private int year;
+        private string phone;
+        private string gender;
+
+        public Student(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Gender { get => gender; set => gender = value; }
+        public string Phone { get => phone; set => phone = value; }
+        public int Year { get => year; set => year = value; }
+        public string Name { get => name; set => name = value; }
+
+        public string Output()
+        {
+            return String.Format(
+                "Name - {0}, Year - {1}, Phone - {2}, Gender - {3}",
+                this.Name,
+                this.Year,
+                this.Phone,
+                this.Gender
+            );
+        }
+    }
+}
